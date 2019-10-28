17e547455d033637f3bb6868fdd9265b400c50da
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-28 12:21:01 +0000

    Initialized server.js code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// implement your API here
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+const db = require('./data/db.js');
+
+server.use(cors());
+
+server.post('/api/users', (req, res) => {
+	
+});
+
+server.get('*', (req, res) => {
+	console.log('cool')
+});
+
+server.listen(process.env.PORT || 3000, () => {
+	console.log('Listening at port', (process.env.PORT || 3000));
+});
