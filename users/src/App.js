7da0b4d552993f8473583ef72c00376781a100c3
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-28 13:36:12 +0000

    Stretch: Added remove user button functionality

--- users/src/App.js
+++ users/src/App.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 function App() {
 	const [users, setUsers] = useState([]);
+
 	useEffect(() => {
 		axios.get('http://localhost:4000/api/users').then(response => {
 			setUsers(response.data);
 		}).catch(err => console.log(err));
 	}, []);
+
 	const addUser = e => {
 		const newUser = {
 			name: "Oladimeji",
@@ -17,6 +19,13 @@ function App() {
 			setUsers([...users, { ...newUser, id: response.data.id }]);
 		}).catch(err => console.log(err));
 	}
+	
+	const removeUser = id => e => {
+		axios.delete('http://localhost:4000/api/users/' + id).then(response => {
+			setUsers(users.filter(user => Number(user.id) !== Number(id)));
+		}).catch(err => console.log(err));
+	}
+
 	return (
 		<div className="App">
 			{
@@ -25,12 +34,12 @@ function App() {
 						<p>ID: {user.id}</p>
 						<p>Name: {user.name}</p>
 						<p>Bio: {user.bio}</p>
-						<button className="removeUser" onClick={removeUser}>Remove user</button>
+						<button className="removeUser" onClick={removeUser(user.id)}>Remove</button>
 					</div>
 				)) : null
 			}
+			<br />
 			<button className="addUser" onClick={addUser}>Add user</button>
-			
 		</div>
 	);
 }
