09d9c843a1b27482dd09e977b584f4efe817ed08
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-28 12:45:20 +0000

    Added base endpoints

--- index.js
+++ index.js
@@ -8,11 +8,76 @@ const db = require('./data/db.js');
 server.use(cors());
 
 server.post('/api/users', (req, res) => {
-	
+	const user = req.params;
+	if (!user.name || !user.bio) {
+		res.status(400).json({
+			errorMessage: "Please provide name and bio for the user."
+		});
+	} else {
+		db.insert({
+			name: user.name,
+			bio: user.bio,
+		}).then(data => {
+			console.log('response', data);
+			res.status(201).json(data);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({ error: "There was an error while saving the user to the database" })
+		});
+	}
+});
+
+server.get('/api/users', (req, res) => {
+	db.find().then(data => {
+		res.status(201).json(data);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({ error: "The users information could not be retrieved." })
+	});
+});
+
+server.get('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	if(!id || Number(id) < 0) {
+		res.status(404).json({ message: "The user with the specified ID does not exist." });
+	}
+	db.findById(id).then(data => {
+		res.status(201).json(data);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({ error: "The user information could not be retrieved." })
+	});
+});
+
+server.delete('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	if(!id || Number(id) < 0) {
+		res.status(404).json({ message: "The user with the specified ID does not exist." });
+	}
+	db.remove(id).then(data => {
+		res.status(201).json(data);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({ error: "There was an error while saving the user to the database" })
+	});
+});
+
+server.put('/api/users/:id', (req, res) => {
+	const user = req.params;
+	db.update(user.id, {
+		name: user.name,
+		bio: user.bio,
+	}).then(data => {
+		console.log('response', data);
+		res.status(201).json(data);
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({ error: "There was an error while saving the user to the database" })
+	});
 });
 
 server.get('*', (req, res) => {
-	console.log('cool')
+	res.status(404).json({ errorMessage: 'Page not found!' })
 });
 
 server.listen(process.env.PORT || 3000, () => {
